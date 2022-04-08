c4d2884f0d3845d44089358b24d66a1779bc4c06
Yura <bolotinya@mail.ru>
2022-04-08 21:40:45 +0300

    Выполненное омашнее задание к уроку 1.10 (доработка)

--- src/com/bolotin/Author.java
+++ src/com/bolotin/Author.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class Author {
     private String name;
     private String surname;
-    private Author FullName;
+
 
     public Author(String name, String surname) {
         this.name = name;
@@ -33,11 +33,11 @@ public class Author {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Author author = (Author) o;
-        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname) && Objects.equals(FullName, author.FullName);
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, surname, FullName);
+        return Objects.hash(name, surname);
     }
 }
